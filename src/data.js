9a5348f17820add03dc4e0c99861d868dbfd8c75
Båmi Lee <swedenebomi@gmail.com>
2022-08-04 18:18:02 +0200

    changes
    Signed-off-by: Båmi Lee <swedenebomi@gmail.com>

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    products: [
+        {
+            name:'Polo Slim shirt',
+            slug: 'Polo-Slim-shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Polo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        }
+    ]
+}
